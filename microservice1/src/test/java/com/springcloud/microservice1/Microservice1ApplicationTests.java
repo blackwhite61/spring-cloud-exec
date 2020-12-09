54f362dedfc197c05fbacbf45f95722732d980f2
baixy <baixy@csii.com.cn>
2020-12-09 13:45:38 +0800

    Initial commit

---
+++ microservice1/src/test/java/com/springcloud/microservice1/Microservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.microservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
